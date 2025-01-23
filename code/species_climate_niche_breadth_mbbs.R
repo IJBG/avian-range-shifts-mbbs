06f7847ddf1838e723c2585cbeb0c0d26ae3fa45
IJBG <ivaragoulden@gmail.com>
2025-01-23 15:15:30 -0500

    trim testing code

--- code/species_climate_niche_breadth_mbbs.R
+++ code/species_climate_niche_breadth_mbbs.R
@@ -107,48 +107,6 @@ for(wc in 1:length(climatelayerslist)) {
   } # end bird data loop
 }
 
-#climatelayers <- getData('worldclim', var='bio', res=10, path=tempdir())
-#climatelayers_ss = climatelayers[[c(5, 10, 18)]]
-#WorldClim doesn't work and can't change the URL (which doesn't exist now) that it's pulling from. Shift to using geodata download, but geodata downloads only worldclim 2_1, and doesn't support previous versions. Which is causing problems with matching data.
-#OH. The paper Grace links uses worldclim 2.1 anyway. huh, soo. don't need to use 1.4 at all? Okay. so. let's just try switching the res to 1k/30s and see if that doesn't fix this mismatch. Ignore using 1.4
-#climatelayers <- geodata::worldclim_global(var = "bio", res = 0.5, path = tempdir())
-#welp, no worries, changing the URL didn't fix it. That's okay. Instead, we'll just load it in. Downloaded from: url = "https://geodata.ucdavis.edu/climate/worldclim/1_4/grid/cur/bio_10m_bil.zip"
-#1.4 download, load in
-  #climatelayers <- list.files("raw_data/bio_10m_bil/", pattern = ".bil$", full.names = TRUE)
-  climatelayers <- list.files("raw_data/wc2.1_30s/", pattern = ".tif$", full.names = TRUE)
-  climatelayers <- stack(climatelayers)
-climatelayers_ss = climatelayers[[c(5, 10, 18)]] #warm quarter max, mean, and precipitation
-#so, previous download used version 1.4 (cannot open URL 'https://biogeo.ucdavis.edu/data/climate/worldclim/1_4/grid/cur/bio_10m_bil.zip') and geodata supports only 2.1 at the moment. We will just re-calc all these scores for all the birds and take a look at how they match up with prev.
-#New download link is https://geodata.ucdavis.edu/climate/worldclim/1_4/grid/cur/bio_10m_bil.zip
-#Okay so Northern Cardinal doesn't match. With Grace's original work it was caluclated at 1.099151035 and with the new version of worldclim it's calculated at 0.374591968
-#soooo, okay now it's downloading 1.4 but it's still NOT getting a match. Now cardinal hypervolume is being calculated as 4.649983191.
-#Is the problem the layers I'm trimming? Are those not the right layers? (5,10,18) - check that they layers haven't moved and confirm from Grace's paper what layer designations she INTENDED to use. Nope, these are the right ones.
-#"To measure climate niche breadth, we calculated a hypervolume for each species using warmest quarter temperature and precipitation variables at 1-km resolution from BioClim"
-#Ah! At the 1-km resolution! I think I've accidentally downloaded from the 10 minute.
-#Okay so, not exactlyyyyy 1km, but I think 30 seconds is what she must have meant.
-#But the specs here are wrong, because it is trying to download the 10m version from Worldclim. This info abt how to do this comes from the hypervolume package
-
-
-#At the LEAST, I need to use the 1.4 to confirm I can replicate the Cardinal calculation. From there, if they match, we can see about if I then switch it to the new worldclim that includes an extra 30 years of climate data or not. 
-#LOL the geodata server is down for maintence and expected back by Dec 21. pft.
-#AH. this may ALSO be why the above link is not working, because the server is down.
-#Yes :) Once the server came back up the path worked perfectly.
-
-# WorldClim crs
-bio_crs <- st_crs(climatelayers)
-
-# z-transform climate layers to make axes comparable
-for (i in 1:nlayers(climatelayers_ss)) {
-  climatelayers_ss[[i]] <- (climatelayers_ss[[i]] - cellStats(climatelayers_ss[[i]], 'mean')) / cellStats(climatelayers_ss[[i]], 'sd') 
-}
-#seems like this worked okay, like climatelayers_ss[[1]] looks like it's still got plenty of information in it.
-
-#this doesn't work, and not sure what's going wrong, but theoretical terra implementation. If something goes wrong below and numbers don't turn out as expected..come back to this area to check
-#for (i in 1:nlyr(climatelayers_ss)) {
-#  climatelayers_ss[[i]] <- (climatelayers_ss[[i]] - terra::global(climatelayers_ss[[i]], 'mean', na.rm = TRUE)) / terra::global(climatelayers_ss[[i]], 'sd', na.rm = TRUE) 
-#}
-
-climatelayers_ss_cropped = crop(climatelayers_ss, extent(-150,-50,15,60)) #this extent is most of North America (minus some parts of Canada), most of South America (except the tip) and some of Africa and Europe. 
 
 ### Climate hypervolume function
 ### Input: species file path for range shapefile
@@ -196,46 +154,5 @@ climate_hypervolume <- function(species) {
   
 
 }
-
-## Calculate climate hypervolume based on each species range map
-
-#this is done so nicely, but is causing problems! let's skip dplyr and do a simple loop.
-spp_list$climate_vol <- 999
-
-#for(i in 10:nrow(spp_list)){
-#cardinal testing
-for(i in 4){
-  
-  species <- spp_list$file[i]
-  print(species)
-  
-  climate_vol <- climate_hypervolume(species)
-  
-  spp_list$climate_vol[i] <- climate_vol
-  
-  #and actually! let's save ALONG THE WAY
-  write.csv(spp_list,
-            paste0(output_dir, "climate_niche_breadth_mbbs_bioclim2.1_30s_upto_", spp_list$spp_name[i], ".csv"),
-            row.names = F)
-  
-}
-
-
-
-
-
-spp_hypervol <- spp_list %>%
-  filter(!is.na(file)) %>%
-  filter(file != "Alauda_arvensis_22717415.shp", file != "Phylloscopus_borealis_22715316.shp") %>%
-  mutate(climate_vol = map_dbl(file, ~climate_hypervolume(.))) %>%
-  dplyr::select(file, spp_name, file_binomial, aou, english_common_name, family, genus, species, binomial, old_genus, 
-                new_binomial, matched_filename, species_code, climate_vol)
-  
-write.csv(spp_hypervol,
-            paste0(output_dir, "climate_niche_breadth_mbbs_ACFL.csv"),
-            row.names = F)
-
-
-#okay well, it's stalling out. We should cut the species list down to one species n see if it's actually broke or just taking a long time to run bc ""lots"" (not THAT many lots) of species
 
 
